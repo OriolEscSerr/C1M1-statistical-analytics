732c2147b9832d3b2a0bba38ea7d421496808a73
OriolEscSerr <oriolescserr@gmail.com>
2025-07-16 16:14:42 +0200

    Fix error: Sorting is now descending

--- stats.c
+++ stats.c
@@ -85,12 +85,12 @@ int find_minimum(unsigned char *array, unsigned int length) {
 
 void sort_array(unsigned char *array, unsigned int length) {
   for (unsigned int i = 0; i < length - 1; i++) {
-    for (unsigned int j = 0; j < length - i - 1; j++) {
-      if (array[j] > array[j + 1]) {
-        // Swap the elements
-        unsigned char temp = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = temp;
+    for (unsigned int j = i + 1; j < length; j++) {
+      if (array[i] < array[j]) {
+        // Swap elements
+        unsigned char temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
       }
     }
   }
